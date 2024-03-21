84fcfe3ec9b0362c4abe6a062d67d5914c25d22c
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-21 18:35:07 +0500

    modified work

--- main.js
+++ main.js
@@ -2,16 +2,38 @@ let input = document.querySelector('input');
 let ol = document.querySelector('ol');
 let emptyArray = [];
 
-function renderScreen(){
+function addfun() {
+    ol.innerHTML = ' '
+    for (let i = 0; i < emptyArray.length; i++) {
+        ol.innerHTML += `<li>${emptyArray[i]}
+        <img onclick="deleteTodo(${i})" src="./Assets/Images/Delete.png" alt="">
+        <img onclick="editTod(${i})" src="./Assets/Images/edit.png" alt="">
+        </li>
+        `
+    }
+}
+
+function renderScreen() {
     ol.innerHTML = ' '
     emptyArray.push(input.value)
     console.log(emptyArray);
     input.value = ''
 
-    for (let i = 0; i < emptyArray.length; i++){
+    for (let i = 0; i < emptyArray.length; i++) {
         ol.innerHTML += `<li>${emptyArray[i]}
-        <button>Delete</button>
-        <button>Edit</button>
-        </li>`
+        <img class="icon" onclick="deleteTodo(${i})" src="./Assets/Images/Delete.png" alt="">
+        <img class="icon" onclick="editTodo(${i})" src="./Assets/Images/edit.png" alt="">
+        </li>
+        `
     }
+}
+
+function deleteTodo(index) {
+    emptyArray.splice(index, 1)
+    addfun()
+}
+function editTodo(index) {
+    let editValue = prompt('Enter Ubbdated Value', emptyArray[index])
+    emptyArray.splice(index, 1 , editValue)
+    addfun()
 }
