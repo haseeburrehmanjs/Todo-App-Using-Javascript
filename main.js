634d7b9cb2c20e8dc7d292175b2d3beb30610211
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-09-05 01:11:23 +0500

    todo app practice

--- main.js
+++ main.js
@@ -1,43 +1,90 @@
-let input = document.querySelector('input');
-let ol = document.querySelector('ol');
-let emptyArray = [];
+// let list = document.querySelector('#list')
+// let input = document.querySelector('#input')
+// let form = document.querySelector('#form')
+// let addTodo = []
 
-function addfun() {
-    ol.innerHTML = ' '
-    for (let i = 0; i < emptyArray.length; i++) {
-        ol.innerHTML += `<li>${emptyArray[i]}
-        <img onclick="deleteTodo(${i})" src="./Assets/Images/Delete.png" alt="">
-        <img onclick="editTod(${i})" src="./Assets/Images/edit.png" alt="">
-        </li>
-        `
-    }
-}
+// function renderScreen() {
+//     list.innerHTML = ''
+//     for (let i = 0; i < addTodo.length; i++) {
+//         list.innerHTML += `
+//         <li class="list-style d-flex justify-content-between mt-2">
+//             ${addTodo[i]}
+//            <div>
+//            <button onclick='deleteTodo(${i})'>Delete</button>
+//            <button onclick="editTodo(${i})">Edit</button>
+//            </div>
+//         </li>
+//         `
+//     }
+// }
+
+// function submitData() {
+//     if (input.value === '') {
+//         alert('Enter Your list Items')
+//     } else {
+//         addTodo.push(input.value)
+//         input.value = ''
+//         renderScreen()
+//     }
+// }
+
+
+// function deleteTodo(index) {
+//     addTodo.splice(index , 1)
+//     renderScreen()
+// }
+
+// function editTodo(index){
+//     let ubdateValue = prompt('Ubdate List')
+//     addTodo.splice(index,1,ubdateValue)
+//     renderScreen()
+// }
+
+
+let list = document.querySelector('#list')
+let input = document.querySelector('#input')
+let form = document.querySelector('#form')
+let addTodo = []
 
 function renderScreen() {
-    if (input.value === ''){
-        alert('Enter Your Task');
-    }else {
-        ol.innerHTML = ' '
-    emptyArray.push(input.value)
-    console.log(emptyArray);
-    input.value = ''
-
-    for (let i = 0; i < emptyArray.length; i++) {
-        ol.innerHTML += `<li>${emptyArray[i]}
-        <img class="icon" onclick="deleteTodo(${i})" src="./Assets/Images/Delete.png" alt="">
-        <img class="icon" onclick="editTodo(${i})" src="./Assets/Images/edit.png" alt="">
+    list.innerHTML = ''
+    addTodo.map((item, index) => {
+        // console.log(item);
+        list.innerHTML += `
+        <li class="list-style d-flex justify-content-between align-items-center mt-2">
+            ${item}
+           <div>
+           <button onclick='deleteTodo(${index})'>Delete</button>
+           <button onclick="editTodo(${index})">Edit</button>
+           </div>
         </li>
         `
-    }
-    }
+    })
 }
 
-function deleteTodo(index) {
-    emptyArray.splice(index, 1)
-    addfun()
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    if(input.value === ''){
+        alert('please add todo!')
+    }else {
+        addTodo.push(input.value)
+    console.log(addTodo);
+    renderScreen()
+    input.value = ''
+    }
+})
+
+let deleteTodo = (index) => {
+    addTodo.splice(index, 1)
+    renderScreen()
 }
-function editTodo(index) {
-    let editValue = prompt('Enter Ubbdated Value', emptyArray[index])
-    emptyArray.splice(index, 1 , editValue)
-    addfun()
+
+let editTodo = (index) => {
+    let newTodo = prompt('Enter New Todo', addTodo[index])
+    if(newTodo === ''){
+        renderScreen()
+    }else {
+        addTodo.splice(index, 1, newTodo)
+        renderScreen()
+    }
 }
